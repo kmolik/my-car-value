f0c65f7712c18bfc69f20058a413fe25c74a063d
kmolik <48009091+kmolik@users.noreply.github.com>
2022-08-23 14:24:19 +0200

    first commit

---
+++ src/users/controllers/users.controller.ts
@@ -0,0 +1,86 @@
+import {
+  Body,
+  Controller,
+  Delete,
+  Get,
+  NotFoundException,
+  Param,
+  Patch,
+  Post,
+  Query,
+  Session,
+  UseGuards,
+} from '@nestjs/common';
+import { CreateUserDto } from './dtos/create-user.dto';
+import { UsersService } from './services/users.service';
+import { UpdateUserDto } from './dtos/update-user.dto';
+import { UserDto } from './dtos/user.dto';
+import { Serialize } from '../interceptors/serialize.interceptor';
+import { AuthService } from './services/auth.service';
+import { CurrentUser } from './decorators/current-user.decorator';
+import { User } from './user.entity';
+import { AuthGuard } from '../guards/auth.guard';
+
+@Controller('auth')
+@Serialize(UserDto)
+export class UsersController {
+  constructor(
+    private usersService: UsersService,
+    private authService: AuthService,
+  ) {}
+
+  /*@Get('me')
+  me(@Session() session: any) {
+    return this.usersService.findOne(session.userId);
+  }*/
+
+  @Get('me')
+  @UseGuards(AuthGuard)
+  me(@CurrentUser() user: User) {
+    return user;
+  }
+
+  @Post('/logout')
+  logout(@Session() session: any) {
+    session.userId = null;
+    return true;
+  }
+
+  @Post('/signup')
+  async createUser(@Body() body: CreateUserDto, @Session() session: any) {
+    const user = await this.authService.signup(body.email, body.password);
+    session.userId = user.id;
+    return user;
+  }
+
+  @Post('/login')
+  async login(@Body() body: CreateUserDto, @Session() session: any) {
+    const user = await this.authService.login(body.email, body.password);
+    session.userId = user.id;
+    return user;
+  }
+
+  @Get('/:id')
+  async findUser(@Param('id') id: string) {
+    const user = await this.usersService.findOne(parseInt(id));
+    if (!user) {
+      throw new NotFoundException('User not found');
+    }
+    return user;
+  }
+
+  @Get()
+  findAllUsers(@Query('email') email: string) {
+    return this.usersService.find(email);
+  }
+
+  @Delete('/:id')
+  removeUser(@Param('id') id: string) {
+    return this.usersService.remove(parseInt(id));
+  }
+
+  @Patch('/:id')
+  updateUser(@Param('id') id: string, @Body() body: UpdateUserDto) {
+    return this.usersService.update(parseInt(id), body);
+  }
+}
---
+++ src/users/entities/user.entity.ts
@@ -0,0 +1,33 @@
+import {
+  AfterInsert,
+  AfterRemove,
+  AfterUpdate,
+  Column,
+  Entity,
+  PrimaryGeneratedColumn,
+} from 'typeorm';
+
+@Entity()
+export class User {
+  @PrimaryGeneratedColumn()
+  id: number;
+  @Column()
+  email: string;
+  @Column()
+  password: string;
+
+  @AfterInsert()
+  logInsert() {
+    console.log(`User with id ${this.id} has been inserted`);
+  }
+
+  @AfterUpdate()
+  logUpdate() {
+    console.log(`User with id ${this.id} has been updated`);
+  }
+
+  @AfterRemove()
+  logRemove() {
+    console.log(`User with id ${this.id} has been removed`);
+  }
+}
